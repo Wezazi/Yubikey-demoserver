224806e2843c371edba44856c5c94cce2a11611a
Dennis Fokin <dennis.fokin@yubico.com>
2026-03-20 14:29:06 +0100

    Gradle fixes

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,13 +10,13 @@ repositories {
 }
 
 dependencies {
-  implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
+  implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.19.0-rc.2")
   implementation("io.franzbecker:gradle-lombok:5.0.0")
 
   // Spotless dropped Java 8 support in version 2.33.0
   // spotless-plugin-gradle dropped Java <17 support in version 8.0.0
   if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.2.0")
-    implementation("io.github.cosmicsilence:gradle-scalafix:0.2.2")
+    implementation("io.github.cosmicsilence:gradle-scalafix:0.2.6")
   }
 }
